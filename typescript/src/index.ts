cb3989d7820595425d31fb124108cdc577bbddad
Bailig <barrybailig@hotmail.com>
2023-07-01 22:27:35 +0300

    added channel that keeps track of messages and memebers

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -5,46 +5,162 @@ import WebSocket from "ws";
 const server = fastify();
 server.register(ws);
 
-const clients = new Set<WebSocket>();
-const channels = new Map<string, Set<WebSocket>>();
+const sockets = new Set<WebSocket>();
+const channels = new Map<number, Channel>();
+
+class User {
+  private static idCounter = 0;
+  id: number;
+  constructor() {
+    this.id = User.idCounter++;
+  }
+}
+
+class Message {
+  private static idCounter = 0;
+  id: number;
+  ownerId: number;
+  content: string;
+  channelId: number;
+  constructor(ownerId: number, content: string, channelId: number) {
+    this.id = Message.idCounter++;
+    this.ownerId = ownerId;
+    this.content = content;
+    this.channelId = channelId;
+  }
+}
+
+class Channel {
+  private static idCounter = 0;
+  id: number;
+  name = "Channel";
+  private members = new Map<number, User>();
+  private messages = new Map<number, Message>();
+  private messageOrder = new Array<number>();
+
+  constructor(name?: string) {
+    this.id = Channel.idCounter++;
+    if (name) {
+      this.name = name;
+    }
+  }
+
+  join(user: User) {
+    this.members.set(user.id, user);
+  }
+
+  remove(user: User) {
+    this.members.delete(user.id);
+    if (this.members.size === 0) {
+      this.clear();
+    }
+  }
+
+  getMessages() {
+    return this.messageOrder.map((id) => this.messages.get(id));
+  }
+
+  send(message: Message) {
+    this.messages.set(message.id, message);
+    this.messageOrder.push(message.id);
+  }
+
+  getMembers() {
+    return Array.from(this.members.values());
+  }
+
+  getMemberCount() {
+    return this.members.size;
+  }
+
+  private clear() {
+    this.members.clear();
+    this.messages.clear();
+    this.messageOrder = [];
+  }
+}
+
+type SocketMessage =
+  | {
+      type: "join";
+      payload: {
+        channelId: number;
+        channelName?: string;
+        userId?: number;
+      };
+    }
+  | {
+      type: "send";
+      payload: {
+        channelId: number;
+        messageContent: string;
+        userId?: number;
+      };
+    }
+  | {
+      type: "leave";
+      payload: {
+        channelId: number;
+        userId?: number;
+      };
+    };
+
+const users = new Map<number, User>();
+const userSockets = new Map<number, WebSocket>();
 
 server.get("/chat", { websocket: true }, async (connection, request) => {
-  clients.add(connection.socket);
+  sockets.add(connection.socket);
+  const user = new User();
+  users.set(user.id, user);
+  userSockets.set(user.id, connection.socket);
+
   connection.socket.on("message", (message) => {
-    const { type, data } = JSON.parse(message.toString());
+    const { type, payload }: SocketMessage = JSON.parse(message.toString());
     if (type === "join") {
-      const { channelId } = data;
+      const { channelId } = payload;
       if (!channels.has(channelId)) {
-        channels.set(channelId, new Set<WebSocket>());
+        channels.set(channelId, new Channel(payload.channelName));
       }
-      channels.get(channelId)!.add(connection.socket);
-    } else if (type === "send") {
-      const { channelId, message } = data;
-      const channel = channels.get(channelId);
-      if (!channel) {
+      const channel = channels.get(channelId)!;
+      channel.join(user);
+      const socket = userSockets.get(user.id);
+      if (!socket) {
         connection.socket.send(
-          {
-            type: "error",
-            message: "Channel not found!",
-          }.toString()
-        );
-      } else
-        for (const client of channel) {
-          client.send(message);
-        }
-    } else if (type === "leave") {
-      const { channelId } = data;
-      const channel = channels.get(channelId);
-      if (!channel) {
-        connection.socket.send(
-          {
-            type: "error",
-            message: "Channel not found!",
-          }.toString()
+          { type: "error", message: "User not found" }.toString()
         );
       } else {
-        channel.delete(connection.socket);
-        if (channel.size === 0) {
+        socket.send(
+          {
+            type: "joined",
+            payload: { messages: channel.getMessages() },
+          }.toString()
+        );
+      }
+    } else if (type === "send") {
+      const { channelId, messageContent } = payload;
+      const channel = channels.get(channelId);
+      if (!channel) {
+        connection.socket.send(
+          { type: "error", message: "Channel not found!" }.toString()
+        );
+      } else {
+        const message = new Message(user.id, messageContent, channelId);
+        channel.send(message);
+        for (const user of channel.getMembers()) {
+          const socket = userSockets.get(user.id);
+          socket!.send({ type: "message", payload: { message } }.toString());
+        }
+      }
+    } else if (type === "leave") {
+      const { channelId } = payload;
+      const channel = channels.get(channelId);
+      if (!channel) {
+        connection.socket.send(
+          { type: "error", message: "Channel not found!" }.toString()
+        );
+      } else {
+        channel.remove(user);
+        if (channel.getMemberCount() === 0) {
           channels.delete(channelId);
         }
       }
@@ -52,7 +168,9 @@ server.get("/chat", { websocket: true }, async (connection, request) => {
   });
 
   connection.socket.on("close", () => {
-    clients.delete(connection.socket);
+    users.delete(user.id);
+    userSockets.delete(user.id);
+    sockets.delete(connection.socket);
   });
 });
 
